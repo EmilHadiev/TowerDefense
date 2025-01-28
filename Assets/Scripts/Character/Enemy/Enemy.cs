8e1f4968d3f5f0a36c0e96b0a9cc7ac3e6390f13
EmilKhadiev <newemil00@mail.ru>
2025-01-28 21:37:04 +0300

    Add enemy move pattern

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
+using Zenject;
 
 public class Enemy : MonoBehaviour, IHealth
 {
     [SerializeField] private float _health;
+    [SerializeField] private float _speed;
+
+    private IPlayer _player;
+    private IMover _mover;
+
+    private void Start()
+    {
+        _mover = new EnemyMovePattern(_player, _speed, transform);
+        SetMover(_mover);
+    }
+
+    private void Update() => _mover.Update();
+
+    [Inject]
+    private void Constructor(IPlayer player) => _player = player;
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
-        Debug.Log("Урон нанесен!");
 
         if (_health <= 0)
             Die();
     }
 
-    private void Update()
+    public void SetMover(IMover mover)
     {
-        
+        _mover?.StopMove();
+        _mover = mover;
+        _mover.StartMove();
     }
 
     private void Die()
---
+++ Assets/Scripts/Mover/Enemy/EnemyMovePattern.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyMovePattern : IMover
+{
+    private readonly IPlayer _player;
+    private readonly float _speed;
+    private readonly Transform _enemy;
+
+    private bool _isWorking;
+
+    public EnemyMovePattern(IPlayer player, float speed, Transform enemy)
+    {
+        _player = player;
+        _speed = speed;
+        _enemy = enemy;
+    }
+
+    public void StartMove()
+    {
+        _isWorking = true;
+    }
+
+    public void StopMove()
+    {
+        _isWorking = false;
+    }
+
+    public void Update()
+    {
+        if (_isWorking == false)
+            return;
+
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        Vector3 direction = _player.Transform.position - _enemy.position;
+        _enemy.Translate(direction * _speed * Time.deltaTime);
+    }
+}
