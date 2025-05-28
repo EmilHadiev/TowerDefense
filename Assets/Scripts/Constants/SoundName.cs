0b025a3b13f1e25ce3bbf4026a5ee01186828d2c
EmilKhadiev <newemil00@mail.ru>
2025-05-28 17:18:38 +0300

    change enemy move speed

--- Assets/Scripts/Constants/SoundName.cs
+++ Assets/Scripts/Constants/SoundName.cs
@@ -3,6 +3,7 @@ public static class SoundName
     public const string SwitchBullet = "SwitchBullet";
     public const string SpendCoin = "SpendCoin";
     public const string Resurrect = "Resurrect";
+    public const string BarricadeBuild = "BarricadeBuild";
 
     public const string FireExplosion = "FireExplosion";
 }
